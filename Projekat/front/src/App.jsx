0424d34aed22b57896822e3a6da80d186006d356
djordje <djolenosmile@gmai.com>
2025-07-04 19:11:34 +0200

    instalirani

---
+++ Projekat/front/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './index.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import Dashboard from './pages/Dasboard';
+
+import DashboardLayout from './components/Dashboard';
+import Sidebar from './components/layout/Sidebar';
+import Clients from './pages/Clients';
+import InvoicePage from './pages/Invoices';
+import ClientsPage from './pages/Clients';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="flex h-screen overflow-hidden">
+        <div className="flex flex-col md:flex-row flex-1">
+          <Sidebar />
+          <main className="flex-1 overflow-y-auto p-4">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+
+              <Route path="/clients" element={<ClientsPage />} />
+
+              <Route path="/invoices" element={<InvoicePage />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ Projekat/front/src/pages/Dashboard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import DashboardLayout from '../components/layout/DashboardLayout';
+import Sidebar from '../components/layout/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import SettingsPage from './Settings';
+
+function Dasboard() {
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row bg-gray-100">
+      <div className="w-full md:w-64 flex-shrink-0 bg-white shadow-lg">
+        {/* Basic Sidebar */}
+        <Sidebar />
+      </div>
+      <div className="flex-1 p-4 overflow-auto">
+        <div className="container mx-auto">
+          <DashboardLayout />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dasboard;
